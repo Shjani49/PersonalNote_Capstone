e8c75984ac8240844ff268f3967e799dcfd24615
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-10-01 18:51:11 -0600

    Created read notes controller and API files.

---
+++ PersonalNotes/Controllers/NotesAPIController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PersonalNotes.Models;
+
+
+namespace PersonalNotes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesAPIController : ControllerBase
+    {
+        [HttpGet("AllNotes")]
+        public ActionResult<List<Notes>> AllNotes()
+        {
+            // TODO: Catch for unable to connect to database.
+            // Return the response.
+            return new NotesController().GetNotes();
+        }
+    }
+}
